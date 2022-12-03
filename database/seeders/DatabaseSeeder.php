63782680558a6bb95bf75fc60e3c6243d8ac0109
Willie <soo.willie@yahoo.co.id>
2022-12-03 11:08:33 +0700

    call seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(UserSeeder::class);
+        $this->call(ActorSeeder::class);
+        $this->call(MovieSeeder::class);
+        $this->call(GenreSeeder::class);
+        $this->call(MovieActorSeeder::class);
+        $this->call(MovieGenreSeeder::class);
+        $this->call(MovieListSeeder::class);
     }
 }
